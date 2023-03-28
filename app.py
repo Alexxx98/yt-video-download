9ba5be2190e2a5b06dcdde7bf58c02e1065dfcfb
Aleksander Pres <aleksander.pres98@gmail.com>
2023-03-28 16:22:06 +0200

    backend added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from downloader import download_video
 import os
 
 
@@ -8,10 +9,14 @@ app = Flask(__name__)
 
 app.config["UPLOAD_FOLDER"] = PHOTO_FOLDER
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     yt_logo = os.path.join(PHOTO_FOLDER, "youtube-logo.png")
+
+    if request.method == 'POST':
+        url = request.form.get('link')
+        download_video(url)
     return render_template('index.html', youtube_logo=yt_logo)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
---
+++ downloader.py
@@ -0,0 +1,7 @@
+from pytube import YouTube
+
+
+def download_video(url):
+    yt = YouTube(url)
+    stream = yt.streams.get_highest_resolution()
+    return stream.download()
